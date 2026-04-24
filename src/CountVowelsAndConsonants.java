c8e2474ba35aeadd460b118315c23ef4243a7afe
shaima <shaima.anvar@gmail.com>
2026-04-24 18:04:05 -0400

    13. Count vowels and consonants

---
+++ src/CountVowelsAndConsonants.java
@@ -0,0 +1,27 @@
+public class CountVowelsAndConsonants {
+    public static void main(String[] args) {
+
+        String input = "Hello world2";
+        int vowels = 0;
+        int consonants = 0;
+
+        input = input.toLowerCase();
+
+        for (int i = 0; i < input.length(); i++) {
+
+            char ch = input.charAt(i);
+
+            if (Character.isLetter(ch)) {
+
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
